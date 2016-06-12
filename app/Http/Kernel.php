fd56e0007b48a715dd7300b2417d00cf02f18d06
shemi <shemi.perez@gmail.com>
2016-06-13 01:52:35 +0300

    Kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends HttpKernel
      */
     protected $middleware = [
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
-        \Clockwork\Support\Laravel\ClockworkMiddleware::class,
+//        \Clockwork\Support\Laravel\ClockworkMiddleware::class,
     ];
 
     /**
